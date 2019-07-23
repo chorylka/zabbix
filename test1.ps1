219c313d57ecfff94ebd13f043ec2ed480c08223
chorylka <33422017+chorylka@users.noreply.github.com>
2019-07-23 09:31:04 -0300

    Update test1.ps1
    adicionado comentário da função de script

--- test1.ps1
+++ test1.ps1
@@ -1,3 +1,4 @@
+# este script em powershell retorna a quantidade de arquivos da pasta c:/temp; funciona apenas em powershell (Windows)
 $files = Get-ChildItem C:\temp -Recurse | Where-Object {!$_.PSIsContainer} | Measure-Object
 
 $files.Count
